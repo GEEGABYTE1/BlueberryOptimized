17cb5770f00905fda0adf2d003f9eaa82d0a0477
Jaival Patel <pateljaival05@gmail.com>
2022-06-22 10:53:01 -0400

    testing complete - require a UserWallet for Each

--- test/sample-blueberry.js
+++ test/sample-blueberry.js
@@ -19,9 +19,29 @@ async function main() {
         userWallet
     )
     let options = {'gasPrice': 21000, 'gasLimit':320000}
+    /*
     const setTx1 = await Ballot.giveRightToVote('0x127a95027B5c7E1D807433837C9cDD7e6f336803', options) // Error was caused by ABI not being 
     await setTx1.wait()                                                                               // updated with new contracts
-    console.log(setTx1)
+    console.log(setTx1)*/
+
+    /*const setTx2 = await Ballot.delegate('0x127a95027B5c7E1D807433837C9cDD7e6f336803', options)
+    await setTx2.wait()
+    console.log(setTx2)*/
+    
+    const userWallet2 = new hre.ethers.Wallet(process.env.PRIVATE_KEY2, alchemy)
+    const user2Ballot = new hre.ethers.Contract (
+      process.env.CONTRACT_ADDRESS,
+      abi,
+      userWallet2
+    )
+
+    /*const setTx3 = await user2Ballot.vote(0, options)
+    await setTx3.wait()
+    console.log('You have successfully voted!')*/
+
+    const setTx4 = await Ballot.winnerName()
+
+    console.log(setTx4)
 
     
 
